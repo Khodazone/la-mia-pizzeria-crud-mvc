31305a5e420fc1e05ac63faf465e6c5cd690929b
Khodazone <khodazone@gmail.com>
2023-05-18 15:13:08 +0200

    db

---
+++ LaMiaPizzeria/Database/PizzaContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_ef_pizze
+{
+    public class PizzaContext : DbContext
+    {
+        public DbSet<Pizza> Pizza { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=EfPizze;" +
+                "Integrated Security=True;TrustServerCertificate=True");
+        }
+    }
+}
---
+++ LaMiaPizzeria/Models/Pizza.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_ef_pizze
+{
+    public class Pizza
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public double Price { get; set; }
+
+        public string Image { get; set; }
+    }
+}
