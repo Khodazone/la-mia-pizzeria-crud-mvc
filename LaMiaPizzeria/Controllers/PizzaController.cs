c582122c6340d2cd2ed48b90187d4846c4ce625e
Khodazone <khodazone@gmail.com>
2023-05-18 15:43:21 +0200

    start

--- LaMiaPizzeria/Controllers/PizzaController.cs
+++ LaMiaPizzeria/Controllers/PizzaController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using csharp_ef_pizze;
+using LaMiaPizzeria.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LaMiaPizzeria.Controllers
 {
@@ -6,7 +8,31 @@ namespace LaMiaPizzeria.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            using (PizzaContext db = new PizzaContext())
+            {
+                List<Pizza> ourPizze = db.Pizza.ToList();
+                return View(ourPizze);
+            }
+
+
+        }
+
+        public IActionResult Details(int id)
+        {
+            using (PizzaContext db = new PizzaContext())
+            {
+                Pizza? dettagliPizze = db.Pizza.Where(pizze => pizze.Id == id).FirstOrDefault();
+
+                if (dettagliPizze != null)
+                {
+                    return View("Details", dettagliPizze);
+                }
+                else
+                {
+                    return NotFound($"La pizza con id {id} non è stato trovato!");
+                }
+            }
+
         }
     }
 }
